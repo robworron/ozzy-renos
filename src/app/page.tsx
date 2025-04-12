377b0dff68d72e512dc6d63e2f45a8d4f8343cac
rworron <rworron94@gmail.com>
2025-04-12 12:06:20 -0400

    modified icon and page title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,5 +3,9 @@ import Image from "next/image";
 import NavBar from "./components/NavBar";
 
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <NavBar />
+    </>
+  );
 }
