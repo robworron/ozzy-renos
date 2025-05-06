dded17d402f1420f8f8d3b223b04c1a4a21802fc
rworron <rworron94@gmail.com>
2025-05-06 00:37:47 -0400

    minor bug fix on service card button

--- src/app/components/ServiceCard.tsx
+++ src/app/components/ServiceCard.tsx
@@ -29,12 +29,12 @@ const ServiceCard = ({
       <p className="hidden relative z-[2] flex-1 content-center items-center w-[80%] mt-4 text-sm lg:text-lg text-center group-hover:block">
         {serviceCardDescription}
       </p>
-      <div className="hidden relative z-[2] content-center items-center w-[80%] mt-4 text-lg text-center group-hover:block">
+      <div className="hidden relative z-[2] content-center items-center w-[90%] mt-4 text-lg text-center group-hover:block">
         <Link
           key={imageAlt}
           href={`/services/#${imageAlt}`}
           aria-label={`See our ${cardTitle} Service Section`}
-          className="px-4 py-2 bg-[var(--red)] text-white text-base md:text-lg lg:text-xl rounded shadow-xl transitions-colors duration-300 ease-in-out hover:bg-[var(--black)]"
+          className="px-4 py-2 bg-[var(--red)] text-white text-sm lg:text-base rounded shadow-xl transitions-colors duration-300 ease-in-out hover:bg-[var(--black)]"
         >
           See More About {cardTitle}
         </Link>
